1638d9413d6155704fb67baf0587e3b9b01916b2
Manoel <manoelcarvalho1984@gmail.com>
2024-02-23 17:44:58 -0300

    estrutura inicial

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/display-name */
+/* eslint-disable no-undef */
+import "./header.css";
+
+
+
+export default Header = () => {
+    return (
+        <>
+            <header>
+                <section className="scontainer flex">
+                    <figure className="logo">
+                        <img src="" alt="logo" width='100px'/>
+                    </figure>
+                </section>
+            </header>
+        </>
+        
+    )
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import Header from "../../components/header/Header"
+
+export const Home = () => {
+    return (
+        <section>
+            <Header />
+        </section>
+    )
+}
