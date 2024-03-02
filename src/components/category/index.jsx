dbbd5084fbef3ce81324570155a89981adc757ac
Manoel Carvalho <manoelcarvalho1984@gmail.com>
2024-03-02 16:28:00 -0300

    idex

--- src/components/category/index.jsx
+++ src/components/category/index.jsx
@@ -1,34 +1,23 @@
+/* eslint-disable no-undef */
 /* eslint-disable react/jsx-key */
+
 import "./category.css";
-// import { category } from '../../data/data';
+import { category } from '../../data/data';
 
 
 
 
 function Category() {
 
-    const category = [
-        {
-            id: 1,
-            category: "Ciência",
-            title: "Moléculas de água",
-            cover: "../../data/images/01.jpg",
-        },
-        {
-            id: 2,
-            category: "Ciência",
-            title: "Células",
-            cover: "../../data/images/02.jpg",
-        }
-    ]
+   
     return (
         <section className="category">
             <div className="content">
-
+        
                 {category.map((item) => (
                     <div className='boxs'>
                         <div className='box' key={item.id}>
-                            <img src={item.cover} alt='cover' />
+                            <img src={item.cover} alt="cover"/>
                             <div className='overlay'>
                                 <h4>{item.category}</h4>
                                 <p>{item.title}</p>
